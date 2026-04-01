0063bd17b922f0321118148ff5f52b881e81c85a
Akksheetha <akkshee@gmail.com>
2026-04-01 12:23:29 +0530

    Committing the file demo

--- selenium_basics/src/main/java/main/demo.java
+++ selenium_basics/src/main/java/main/demo.java
@@ -2,20 +2,13 @@ package main;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-
 import java.time.Duration;
 
 public class demo {
 
 	public static void main(String[] args) {
 
-		ChromeOptions options = new ChromeOptions();
-		options.addArguments("--headless=new");
-		options.addArguments("--disable-gpu");
-		options.addArguments("--no-sandbox");
-
-		WebDriver driver = new ChromeDriver(options);
+		WebDriver driver = new ChromeDriver();
 
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
@@ -26,14 +19,18 @@ public class demo {
 		} catch (Exception e) {
 			System.out.println("Popup not displayed");
 		}
+
 		WebElement search = driver.findElement(By.name("q"));
 		search.sendKeys("Flipkart", Keys.ENTER);
+
 		String title = driver.getTitle();
-		System.out.println("Page title is: " + title);
-		System.out.println("Title length: " + title.length());
+		System.out.println("Page title is " + title);
+		System.out.println(title.length());
+
 		String currenturl = driver.getCurrentUrl();
-		System.out.println("Current URL is: " + currenturl);
-		System.out.println("Page source length: " + driver.getPageSource().length());
+		System.out.println("Current URL Command is " + currenturl);
+
+		System.out.println("Page source length is " + driver.getPageSource().length());
 
 		driver.quit();
 	}
